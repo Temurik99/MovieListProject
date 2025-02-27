f17261c3d58589be433aff4ffa0aaf6e355de23f
Temuri <temurikapanadze99@gmail.com>
2025-02-26 20:56:10 -0500

    Final Commit

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -15,11 +15,6 @@ public class MovieCollection {
     public MovieCollection() {
         movieList = new ArrayList<>();
         scan = new Scanner(System.in);
-    }
-
-
-    public void start() {
-
 
         readData();  // read data in from the file
 
@@ -28,6 +23,7 @@ public class MovieCollection {
         String menuOption = "";
 
         while (!menuOption.equals("q")) {
+            System.out.println();
             System.out.println("------------ Main Menu ----------");
             System.out.println("- search (t)itles");
             System.out.println("- search (c)ast");
@@ -38,7 +34,7 @@ public class MovieCollection {
             if (menuOption.equals("t")) {
                 searchTitles();
             } else if (menuOption.equals("c")) {
-
+                searchCast();
             } else if (menuOption.equals("q")) {
                 System.out.println("Goodbye!");
             } else {
@@ -46,7 +42,6 @@ public class MovieCollection {
             }
         }
     }
-
 
     private void readData() {
         try {
@@ -74,8 +69,9 @@ public class MovieCollection {
 
     private void searchTitles() {
         ArrayList<Movie> matches = new ArrayList<Movie>();
-        System.out.println("Enter a title search term: ");
+        System.out.print("Enter a title search term: ");
         String term = scan.nextLine();
+        term = term.toLowerCase();
 
         int counter = 0;
         Movie temp;
@@ -109,14 +105,119 @@ public class MovieCollection {
             System.out.println(counter + ". " + match.getTitle());
         }
         System.out.println("Which movie would you like to learn more about?");
-        System.out.println("Enter Number: ");
+        System.out.print("Enter Number: ");
         int num = scan.nextInt();
+        scan.nextLine();
+        System.out.println();
         System.out.println("Title: " + matches.get(num - 1).getTitle());
         System.out.println("Runtime: " + matches.get(num - 1).getRuntime());
         System.out.println("Directed By: " + matches.get(num - 1).getDirector());
         System.out.println("Cast: " + matches.get(num - 1).getCast());
         System.out.println("Overview: " + matches.get(num - 1).getOverview());
         System.out.println("User Rating: " + matches.get(num - 1).getUserRating());
+    }
+
+    private void searchCast() {
+        ArrayList<String> finalActors = new ArrayList<String>();
+        ArrayList<Movie> trackingInfo = new ArrayList<Movie>();
+        ArrayList<Movie> finalMovies = new ArrayList<Movie>();
+        System.out.print("Enter a person to search for (first or last name): ");
+        String term = scan.nextLine();
+        term = term.toLowerCase();
+
+        int counter = 0;
+        String temp;
+        Movie temp2;
+        int m = 0;
+        int t = 0;
+        int q = 0;
+        int check = 0;
+        String theName = "";
+
+        for (int i = 0; i < movieList.size(); i++) {
+            String actors = movieList.get(i).getCast();
+            String[] actorList = actors.split("\\|");
+
+            for (int j = 0; j < actorList.length; j++) {
+                if (actorList[j].toLowerCase().contains(term)) {
+                    for (int k = 0; k < finalActors.size(); k++) {
+                        if (finalActors.get(k).equals(actorList[j])) {
+                            check = 1;
+                        }
+                    }
+                    if (check == 0) {
+                        finalActors.add(actorList[j]);
+                        trackingInfo.add(movieList.get(i));
+                    }
+                    check = 0;
+                }
+            }
+        }
+        if (finalActors.isEmpty()) {
+            System.out.println("No results match your search");
+            return;
+        }
+        for (int x = 1; x < finalActors.size(); x++) {
+            m = x;
+            t = x;
+            while(m != 0 && finalActors.get(m).compareTo(finalActors.get(m - 1)) < 0) {
+                temp = finalActors.get(m);
+                finalActors.set(m , finalActors.get(m - 1));
+                finalActors.set(m - 1, temp);
+                m--;
+                temp2 = trackingInfo.get(t);
+                trackingInfo.set(t , trackingInfo.get(t - 1));
+                trackingInfo.set(t - 1, temp2);
+                t--;
+            }
+        }
+        for (String actor : finalActors) {
+            counter++;
+            System.out.println(counter + ". " + actor);
+        }
+        counter = 0;
+
+        System.out.println("Which would you like to see all movies for?");
+        System.out.print("Enter Number: ");
+        int num = scan.nextInt();
+        scan.nextLine();
+        for (String name : finalActors) {
+            counter++;
+            if (counter == num) {
+                theName = name;
+                break;
+            }
+        }
+        counter = 0;
+        for (int g = 0; g < movieList.size(); g++) {
+            if (movieList.get(g).getCast().contains(theName)) {
+                finalMovies.add(movieList.get(g));
+            }
+        }
+        for (int i = 1; i < finalMovies.size(); i++) {
+            q = i;
+            while(q != 0 && finalMovies.get(q).getTitle().compareTo(finalMovies.get(q - 1).getTitle()) < 0) {
+                Movie temper = finalMovies.get(q);
+                finalMovies.set(q , finalMovies.get(q - 1));
+                finalMovies.set(q - 1, temper);
+                q--;
+            }
+        }
+        for (Movie theMovie : finalMovies) {
+            counter++;
+            System.out.println(counter + ". " + theMovie.getTitle());
+        }
+        System.out.println("Which movie would you like to learn more about?");
+        System.out.print("Enter Number: ");
+        int number = scan.nextInt();
+        scan.nextLine();
+        System.out.println();
+        System.out.println("Title: " + finalMovies.get(number - 1).getTitle());
+        System.out.println("Runtime: " + finalMovies.get(number - 1).getRuntime());
+        System.out.println("Directed By: " + finalMovies.get(number - 1).getDirector());
+        System.out.println("Cast: " + finalMovies.get(number - 1).getCast());
+        System.out.println("Overview: " + finalMovies.get(number - 1).getOverview());
+        System.out.println("User Rating: " + finalMovies.get(number - 1).getUserRating());
     }
 
 }
